3c0c3c0c8765ce37562c7d7242894b92b6395809
jun39 <bbbbb12312339@gmail.com>
2020-05-17 17:01:44 +0900

    added function of getting comedian information

--- wikiSpider/wikiSpider/articleItems.py
+++ wikiSpider/wikiSpider/articleItems.py
@@ -5,7 +5,7 @@ from wikiSpider.items import Article
 class ArticleSpider(CrawlSpider):
     name = 'articleItems'
     allowed_domains = ['wikipedia.org']
-    start_urls = ['https://en.wikipedia.org/wiki/Benevolent_dictator_for_life']
+    start_urls = ['https://ja.wikipedia.org/wiki/%E3%82%AA%E3%83%BC%E3%83%89%E3%83%AA%E3%83%BC_(%E3%81%8A%E7%AC%91%E3%81%84%E3%82%B3%E3%83%B3%E3%83%93)']
     rules = [
         Rule(LinkExtractor(allow='(/wiki/)((?!:).)*$'), callback='parse_items', follow=True),
     ]
@@ -14,6 +14,7 @@ class ArticleSpider(CrawlSpider):
         article = Article()
         article['url'] = response.url
         article['title'] = response.css('h1::text').extract_first()
+        article['script'] = response.
         article['text'] = response.xpath('//div[@id="mw-content-text"]//text()').extract()
         lastUpdated = response.css('li#footer-info-lastmod::text').extract_first()
         article['lastUpdated'] = lastUpdated.replace('This page was last edited on ', '')
--- wikiSpider/wikiSpider/items.py
+++ wikiSpider/wikiSpider/items.py
@@ -12,4 +12,5 @@ class Article(scrapy.Item):
     url = scrapy.Field()
     title = scrapy.Field()
     text = scrapy.Field()
+    script = scrapy.Field()
     lastUpdated = scrapy.Field()
